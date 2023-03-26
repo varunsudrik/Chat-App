62d6718806b51352d3a346032f07aa125061cce4
Varun Sudrik <varunsudrik99@gmail.com>
2023-03-26 21:17:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./Components/ChatFeed";
+import LoginForm from "./Components/LoginForm";
 
 function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <div className="App">
       <ChatEngine
@@ -9,6 +13,8 @@ function App() {
         projectID="aeb3e459-642e-4723-aaad-cadb23f5ac91"
         userName="redgear204"
         userSecret="Qwerty@123"
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        inNewMessege={() => new Audio("./assets/chat.mp3").play}
       ></ChatEngine>
     </div>
   );
